e9c5bb797be979908a982dd39e3d9eef8703bc96
malco147 <piusmalco@yahoo.com>
2023-11-10 10:39:34 +1100

    Created function

--- func.js
+++ func.js
@@ -1,5 +1,18 @@
 function multiple(num1, num2){
     let mult = num1 * num2;
-    return mult;
+        return mult;
+}
+
+function division(num1, num2){
+    let div = num1 / num2;
+    return div;
+
+}
+
+
+
+function divi(num1, num2){
+    let div = num1 - num2;
+    return div;
 
 }
